6608c02b8b2795fe749c5b00a361fee2bf5af308
arun-balakrishnan4409 <42975302+arun-balakrishnan4409@users.noreply.github.com>
2018-10-12 12:10:49 +0530

    Update EmployeeDTO.java

--- employeemicroservice/src/main/java/com/aspiresys/employeemicroservice/dto/EmployeeDTO.java
+++ employeemicroservice/src/main/java/com/aspiresys/employeemicroservice/dto/EmployeeDTO.java
@@ -38,18 +38,30 @@ public class EmployeeDTO {
 	private Integer employeeId;
 	
 	/** The employee name. */
+	@NotNull(message = "Name should not be null")
+	@Size(min = 1, message = "Name should have atleast one character")
+	@Pattern(regexp="([A-Za-z]*[ ])?[A-Za-z]{1,}")
 	private String employeeName;
 	
 	/** The employee SL. */
+	@NotNull(message = "Service Line should not be null")
+	@Size(min = 1, message = "Service Line should have atleast one character")
+	@Pattern(regexp="([A-Za-z]*[ ])?[A-Za-z]{1,}")
 	private String employeeSL;
 	
 	/** The leave list. */
 	private List<LeaveDTO> leaveList = new ArrayList<>();
 	
 	/** The employee project. */
+	@NotNull(message = "Project should not be null")
+	@Size(min = 1, message = "Project should have atleast one character")
+	@Pattern(regexp="([A-Za-z]*[ ])?[A-Za-z]{1,}")
 	private String employeeProject;
 	
 	/** The employee blood group. */
+	@NotNull(message = "Blood group should not be null")
+	@Size(min = 2, message = "Blood group should have atleast two characters")
+	@Pattern(regexp="^(A|B|AB|O)[+-]$")
 	private String employeeBloodGroup;
 
 
